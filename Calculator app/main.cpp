56dba6d0f6454f6d4742889bc2b741032145fcab
Malachi <mpeel9938@gmail.com>
2023-07-06 15:19:41 -0700

    fixed text functions

--- Calculator app/main.cpp
+++ Calculator app/main.cpp
@@ -20,37 +20,59 @@ public:
         shape.setFillColor(sf::Color(30,30,30));
         shape.setOutlineThickness(5); 
         shape.setOutlineColor(sf::Color(135,206,235));
-        shape.setPosition(position);         
+        shape.setPosition(position);      
 
-    }
 
-    void setText(string str, sf::Vector2f position) {
-
-        sf::Font font; 
-        if (!font.loadFromFile("C:\Fonts")) {
-            cout << "Font load error"; 
-            return; 
-        }
-
-        text.setFont(font);
-        text.setCharacterSize(10); 
-        text.setFillColor(sf::Color(30,30,30)); 
-        text.setPosition(position); 
     }
 
 
     void draw(sf::RenderWindow& window) {
         window.draw(shape);
-        window.draw(text); 
     }
-    
 
     
 private:
 
     sf::RectangleShape shape;
-    sf::Text text; 
 };
+
+
+class Text {
+public:
+
+
+    void setText(string str,  sf::Vector2f position) {
+
+
+        if (!font.loadFromFile("C:\\Fonts\\JAi.TTF")) {
+            cout << "Font load error";
+            return;
+        }
+
+        sf::Text text;
+
+        text.setFont(font);
+        text.setString(str);
+        text.setCharacterSize(50);
+        text.setFillColor(sf::Color::White);
+        text.setPosition(position);
+
+        drawText = text;
+
+
+    }
+
+    void draw(sf::RenderWindow& window) {
+          window.draw(drawText);
+    }
+
+private: 
+    sf::Text drawText;
+    sf::Font font;
+
+
+};
+
 
 //user interface
 class Display {
@@ -65,18 +87,34 @@ public:
         window.setFramerateLimit(60); 
 
         //each button needs a 75x 
-       // Buttons button[13], util_button[5], text[11];
-        std::vector<Buttons> button(13), util_button(5), text(11); 
+        std::vector<Buttons> button(13), util_button(5);
+        std::vector<Text> textBox(12);
 
         //size of numbered buttons
         float x = 75;
         //minimum y position 
         float posY = 105;
 
+
+
+
+
         //main screen
         button[0].button({ 300,100 }, { 0.f, 0.f });
         //first collum
         button[1].button({ x,x }, { 0.f, posY });
+
+        /*
+        // Get the local bounds of the text
+        sf::FloatRect textBounds = textBox[1].getLocalBounds();
+
+        // Calculate the position to center the text within the shape
+        float xPos = button[1].getPosition().x + (button[1].getSize().x - textBounds.width) / 2.f;
+        float yPos = button[1].getPosition().y + (button[1].getSize().y - textBounds.height) / 2.f;
+        textBox[1].setText("1", { 0.f,  posY });
+        */
+      //  textBox[1].setText("1", { (x/2) - 25,  posY });
+
         button[2].button({ x,x }, { 0.f, posY + 80 });
         button[3].button({ x,x }, { 0.f, posY + 160 });
         button[4].button({ x,x }, { 0.f, posY + 240 });
@@ -97,9 +135,7 @@ public:
         util_button[2].button({ x,45 }, { 240.f, posY + 110 });
         util_button[3].button({ x,45 }, { 240.f, posY + 165 });
         util_button[4].button({ x,95 }, { 240.f, posY + 220 });
-       
-
-
+        
         // Enable anti-aliasing for the square
         sf::ContextSettings settings;
         settings.antialiasingLevel = 8; 
@@ -107,8 +143,9 @@ public:
         //window loop
         while (window.isOpen())
         {
+            window.clear(); 
 
-          
+       
             // check all the window's events that 
             //were triggered since the last iteration of the loop
             while (window.pollEvent(event))
@@ -138,18 +175,22 @@ public:
                 default:
                     break;
                 }
-
-                
-                for (int i = 0; i < 13; i++) {
-                    button[i].draw(window); 
-            }
-                for (int i = 0; i < 5; i++) {
-                    util_button[i].draw(window);
-                }
                
-                window.display();
 
             }
+
+
+            for (int i = 0; i < 13; i++) {
+                button[i].draw(window);
+            }
+            for (int i = 0; i < 5; i++) {
+                util_button[i].draw(window);
+            }
+            textBox[1].draw(window);
+
+
+            window.display();
+
 
         }
     }
@@ -175,8 +216,25 @@ int main()
     Display display; 
     display.mainDisplay(); 
 
+    /*
+    Text textBox;
 
-   
+    sf::RenderWindow window(sf::VideoMode(300, 425), "Calculator");
+
+    while (window.isOpen())
+    {
+        window.clear();
+
+        textBox.setText("Hello", { 100.f,  100.f });
+
+        textBox.draw(window); 
+
+        window.display(); 
+
+
+    }
+
+   */
 
     return 0;
 }
